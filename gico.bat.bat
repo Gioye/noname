ca2a3140a66c7671fa09afdd65bb848308ea88db
Gioye <100699274+Gioye@users.noreply.github.com>
2022-03-02 20:37:10 +0100

    Add files via upload

---
+++ gico.bat.bat
@@ -0,0 +1,13 @@
+@echo off
+color 03
+echo Gioye+
+echo MS-DOS
+echo %random%
+echo %random%
+echo %random%
+echo now loading
+echo ------------------------------------------------------
+echo servercode: 90%random%998
+echo now you can close this progamm.
+pause
+
